e944ad1e2f974dd35d2cd9681f3408b80fc29439
Gleb Helgeson <glebh@live.no>
2022-06-18 17:50:55 +0200

    Some initial layout

---
+++ src/components/Container.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Container.css';
+
+function Container() {
+  return (
+    <div className="layout">
+        <div className="main-container">
+            <div className="search">Search</div>
+            <div className="present">Present</div>
+        </div>
+    </div>
+  );
+}
+
+export default Container;
